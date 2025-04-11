67bc254186931284bc7433fa73a3625bb5f30be4
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-12 01:49:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ apt install nano -y
 echo "wget and starting compiling"
 mkdir stb-cpuminer && cd stb-cpuminer
 wget https://github.com/jeptidaeng/stb-cpuminer/raw/main/cpuminer -4
-wget https://github.com/jeptidaeng/stb-cpuminer/raw/main/arm64.de -4
+wget https://github.com/jeptidaeng/stb-cpuminer/raw/main/arm64.deb -4
 wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/mine_avian.sh
 chmod +x stb-cpuminer mine_avian.sh
 echo "  => done."
