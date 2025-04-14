19e9bc9f1ff81cb25a79c6b3e7ef49392fe3e4e4
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-14 13:07:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ apt install wget -y
 apt install nano -y
 echo "wget and starting compiling"
 mkdir cpuminer && cd cpuminer
-wget https://github.com/rdsp87/dero-stb/raw/main/lib.deb -4 sudo dpkg -i lib.deb
-wget https://github.com/jeptidaeng/stb-cpuminer/raw/main/cpuminer -4 
+wget https://github.com/rdsp87/dero-stb/raw/main/lib.deb sudo dpkg -i lib.deb
+wget https://github.com/jeptidaeng/stb-cpuminer/raw/main/cpuminer 
 wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/mine_avian.sh
 chmod +x cpuminer mine_avian.sh
 echo "  => done."
