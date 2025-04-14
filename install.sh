b34e3189459979c34c33261932ca7cf60276da5d
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-14 15:18:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "wget and starting compiling"
 mkdir cpuminer && cd cpuminer
 wget https://github.com/rdsp87/dero-stb/raw/main/lib.deb -4
 wget https://github.com/jeptidaeng/stb-cpuminer/raw/main/cpuminer -4 
-wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/mine_avian.sh
+wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/miner.sh
 chmod +x cpuminer mine_avian.sh
 echo "  => done."
 	else
@@ -26,5 +26,5 @@ strip -s ccminer
 [ $? = 0 ] || exit $?
 echo "  => done."
 else
-   echo "Done... cd ~/cpuminer; ./mine_avian.sh"
+   echo "Done... cd ~/cpuminer; ./miner.sh"
 fi
