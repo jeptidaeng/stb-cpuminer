b2183257ba3dbf6fc8bd79e623e309ee4a2ba3e3
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-18 16:54:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,59 @@
+#!/bin/sh
+sudo apt-get -y update
+sudo apt-get -y upgrade
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev 
+libgmp-dev zlib1g-dev make g++ libtool git screen nano jq wget -y
+wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+mkdir ~/cpuminer
+cd ~/cpuminer
+GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/jeptidaeng/stb-cpuminer/releases?per_page=1" | jq -c '[.[] | del (.body)]')
+GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .browser_download_url")
+GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .name")
+
+echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
+
+wget ${GITHUB_DOWNLOAD_URL} -O ~/cpuminer/cpuminer
+wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/cpuminer-conf.json -O ~/cpuminer/cpuminer-conf.json
+chmod +x ~/cpuminer/cpuminer
+
+cat << EOF > ~/cpuminer/start.sh
 #!/bin/bash
-# Linux build, optimised for ARM devices
 
-if [ ! -r configure ]; then
-	echo "Creating configure..."
-apt-get install curl git wget nano -y
-apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool -y
-echo "wget and starting compiling"
-mkdir cpuminer && cd cpuminer
-wget https://github.com/rdsp87/dero-stb/raw/main/lib.deb -4
-wget https://github.com/jeptidaeng/stb-cpuminer/raw/main/cpuminer -4 
-wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/miner.sh
-chmod +x cpuminer miner.sh
-echo "  => done."
-	else
-		exit 1
-	fi
-if [ $? != 0 ]; then
-echo '$ ls -l cpuminer'
-ls -l cpuminer
+mine_xna() {
+# Mining Algorithm
+ALGO=minotaurx
 
-echo "Stripping..."
+# Pool URL #
+POOL=stratum+tcp://minotaurx.sea.mine.zpool.ca:7019
 
-strip -s ccminer
+# Wallet address to mine (AVN coin recomended)#
+WALLET=RnVU6HeXBDhA3DFFBaNrcqZynusVFkToo
 
-[ $? = 0 ] || exit $?
-echo "  => done."
-else
-   echo "Done... cd ~/cpuminer; ./miner.sh"
-fi
+# Pool Password (Optional) #
+PASS="Userland"
+
+# Mining thread to use #
+# To know how many your own cpu threads, #
+# Type lscpu on the terminal #
+THR=8
+
+# Miner config[Do not edit if you not understand] #
+
+clear
+./cpuminer -a $ALGO -o $POOL -u $WALLET -p $PASS -t $THR
+}
+mine_xna
+EOF
+chmod +x start.sh
+
+echo "setup nearly complete."
+echo "Edit the config with \"nano ~/cpuminer/cpuminer-conf.json\""
+
+echo "go to line 15 and change your worker name"
+echo "use \"<CTRL>-x\" to exit and respond with"
+echo "\"y\" on the question to save and \"enter\""
+echo "on the name"
+
+echo "start the miner with \"cd ~/cpuminer; ./start.sh\"."
