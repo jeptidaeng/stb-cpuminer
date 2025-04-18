a222f2c73381567f7eccc64a4c7dd888d93fc49f
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-18 18:36:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,10 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev 
-libgmp-dev zlib1g-dev make g++ libtool git screen nano jq wget -y
+libgmp-dev zlib1g-dev make g++ libtool git screen nano curl wget -y
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-mkdir ~/cpuminer
-cd ~/cpuminer
-GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/jeptidaeng/termux-miner/releases?per_page=1" | jq -c '[.[] | del (.body)]')
-GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .browser_download_url")
-GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[183538731].assets | .[] | .name,label")
-
+mkdir ~/cpuminer cd ~/cpuminer
 echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME,LABEL"
-
 wget ${GITHUB_DOWNLOAD_URL}
 wget https://raw.githubusercontent.com/jeptidaeng/termux-miner/main/cpuminer
 wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/cpuminer-conf.json 
