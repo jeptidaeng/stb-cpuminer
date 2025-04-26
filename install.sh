944ead6c680041e87b4dbfd2abc4f353c8dea7da
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-26 12:36:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-apt install update -y
-apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev 
-libgmp-dev zlib1g-dev make g++ libtool -y
+ sudo apt install update -y
 wget http://ports.ubuntu.com/pool/main/o/openssl/linux-static_amd64.deb
 mkdir ~/cpuminer
 cd ~/cpuminer
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev 
+libgmp-dev zlib1g-dev make g++ libtool -y
 GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/jeptidaeng/stb-cpuminer/releases?per_page=1" | jq -c '[.[] | del (.body)]')
 GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .browser_download_url")
 GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .name")
