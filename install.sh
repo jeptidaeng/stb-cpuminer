20a9c38501825fd65c2388c9413319286ec76d2c
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-26 14:54:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ mkdir cpuminer && cd cpuminer
 wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/build-linux-arm.sh
 wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/cpuminer-conf.json
 wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/start.sh
-chmod +x build-linux-arm.sh cpuminer-conf.json start.sh
+wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/autogen.sh
+chmod +x autogen.sh build-linux-arm.sh cpuminer-conf.json start.sh
 apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool -y
 # Cmake
 echo "Basic *nix build instructions"
