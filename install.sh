1c6a30b2ceb136ad000b0b5a5b1ed5e13bf1d7ad
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-26 12:29:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
+apt install wget -y
 apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev 
-libgmp-dev zlib1g-dev make g++ libtool git screen nano wget -y
+libgmp-dev zlib1g-dev make g++ libtool -y
 wget http://ports.ubuntu.com/pool/main/o/openssl/linux-static_amd64.deb
 mkdir ~/cpuminer
 cd ~/cpuminer
