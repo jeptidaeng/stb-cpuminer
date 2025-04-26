62e30f42b0039608aae77d82e5b9fe683eacd8fb
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-26 16:02:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,22 +2,19 @@
 
 set -e
 echo "Updating and installing required things"
-apt install wget cmake nano cmake -y
+apt install wget cmake nano -y
 echo "Cloning and starting wget compiling"
-mkdir cpuminer && cd cpuminer
-wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/build-linux-arm.sh
-wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/cpuminer-conf.json
+https://github.com/wong-fi-hung/termux-miner.git
+cd termux-miner
 wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/start.sh
-wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/autogen.sh
-chmod +x autogen.sh build-linux-arm.sh cpuminer-conf.json start.sh
 apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool -y
-# Cmake
+# build-linux-arm
 echo "Basic *nix build instructions"
-if ! bash autogen.sh ..; then
+if ! ./build-linux-arm.sh ..; then
     echo "Error: CMake configuration failed"
     exit 1
 else
-    ./build-linux-arm.sh ..
+     ..
 fi
 # compile
 echo "Compiling now"
