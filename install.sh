c2bf3de6414e04d78e452479b68e0e341f73664a
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-26 14:49:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -e
 echo "Updating and installing required things"
-apt install wget cmake nano -y
+apt install wget cmake nano cmake -y
 echo "Cloning and starting wget compiling"
 mkdir cpuminer && cd cpuminer
 wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/build-linux-arm.sh
@@ -10,7 +10,7 @@ wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/cpuminer-con
 wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/start.sh
 chmod +x build-linux-arm.sh cpuminer-conf.json start.sh
 apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool -y
-# build-linux-arm
+# Cmake
 echo "Basic *nix build instructions"
 if ! ./build-linux-arm.sh ..; then
     echo "Error: CMake configuration failed"
@@ -20,7 +20,7 @@ else
 fi
 # compile
 echo "Compiling now"
-if ! build-linux-arm; then
+if ! Cmake; then
     echo "Error: Compiling failed"
     exit 1
 else
