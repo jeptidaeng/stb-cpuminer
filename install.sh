252fb99dd0412455acb918cc9a250b4184bcdd23
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-26 12:41:32 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
- 
-wget http://ports.ubuntu.com/pool/main/o/openssl/linux-static_amd64.deb -y
+ apt install wget
+wget http://ports.ubuntu.com/pool/main/o/openssl/linux-static_amd64.deb
 mkdir ~/cpuminer
 cd ~/cpuminer
 apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev 
