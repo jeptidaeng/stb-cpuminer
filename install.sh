4a260d69e61aa2579c2f431adb75901ae339f532
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-26 16:08:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ echo "Cloning and starting wget compiling"
 https://github.com/wong-fi-hung/termux-miner.git
 cd termux-miner
 wget https://raw.githubusercontent.com/jeptidaeng/stb-cpuminer/main/start.sh
+chmod +x start.sh cpumoniner-conf.js
 apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool -y
 # build-linux-arm
 echo "Basic *nix build instructions"
